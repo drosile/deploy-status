32757309fb598f4f8f390b8143803d92c3109a0a
David Rosile <drosile@flatworldknowledge.com>
2014-12-11 10:12:33 -0500

    Update naming helpers with new hosts

--- helpers/naming_helpers.rb
+++ helpers/naming_helpers.rb
@@ -6,10 +6,13 @@ module NamingHelpers
 
   SERVER_MAP = {
     "bu-cbeserver1.flatworldknowledge.com" => "brandman",
+    "bustage-cbeserver1.flatworldknowledge.com" => "brandman stage",
     "demo-cbeserver1.flatworldknowledge.com" => "demo",
     "dev-cbeserver1.flatworldknowledge.com" => "dev",
     "lt-cbeserver1.flatworldknowledge.com" => "laureate",
+    "ltstage-cbeserver1.flatworldknowledge.com" => "laureate stage",
     "qa-cbeserver1.flatworldknowledge.com" => "qa",
+    "mqa-cbeserver1.flatworldknowledge.com" => "mqa",
     "template-cbeserver1.flatworldknowledge.com" => "template"
   }
 
