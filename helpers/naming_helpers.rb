f0941d2c1218169cfb26f2dd288b219f08ab66d6
David Rosile <drosile@flatworldknowledge.com>
2016-03-02 08:36:54 -0700

    Update naming helpers...again

--- helpers/naming_helpers.rb
+++ helpers/naming_helpers.rb
@@ -1,16 +1,21 @@
 module NamingHelpers
   REPO_MAP = {
-    "admin-web" => "id-web",
-    "comp-web" => "cbe-web"
+    "admin-web" => "design",
+    "comp-web" => "learn"
   }
 
   SERVER_MAP = {
     "dev-cbeserver1.flatworld.com" => "dev",
-    "mqa-cbeserver1.flatworld.com" => "mqa",
-    "mtqa-cbeserver1.flatworld.com" => "qa 1",
-    "mtqa-cbeserver2.flatworld.com" => "qa 2",
+    "mqa-cbe1" => "mqa",
+    "qa-cbe1" => "qa 1",
+    "qa-cbe2" => "qa 2",
+    "mdev-cbe1" => "mdev",
+    "sdev-cbe1" => "sdev",
     "mtprod-cbeserver1.flatworld.com" => "prod 1",
-    "mtprod-cbeserver2.flatworld.com" => "prod 2"
+    "mtprod-cbeserver2.flatworld.com" => "prod 2",
+    "fwk-experiments" => "Scholar X",
+    "fwk-scholar1" => "Scholar 1",
+    "fwk-scholar2" => "Scholar 2"
   }
 
   def server_name(server_name)
